8d733823644c6afdfb79a78a5d49e935bb1f0f5f
Faiiruz <fairuz.rafi.yadisa@gmail.com>
2023-11-06 16:25:03 +0700

    update toggle

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,11 +3,27 @@ import Link from "next/link";
 import { RxDashboard } from "react-icons/rx";
 import { TbWorldSearch } from "react-icons/tb";
 import { MdOutlineMarkunreadMailbox } from "react-icons/md";
+import { BsArrowLeftShort } from "react-icons/bs";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const router = useRouter();
+
   return (
-    <div className="w-72 h-screen bg-[#172882]">
+    <div
+      className={`${
+        open ? "w-68" : "w-20"
+      }  h-screen bg-[#172882] duration-300 relative`}
+    >
       <div className="p-4 text-white">
+        <BsArrowLeftShort
+          className={`text-3xl bg-black rounded-full absolute -right-3 top-9 cursor-pointer ${
+            !open && "rotate-180"
+          }`}
+          onClick={() => setOpen(!open)}
+        />
         <div className="flex items-center justify-center mb-7">
           <Image src="/image/OIG1.png" width={100} height={100} />
         </div>
@@ -15,28 +31,40 @@ const Sidebar = () => {
           <li>
             <Link
               href="/"
-              className="flex gap-3 font-popin items-center p-2 rounded"
+              className={`flex gap-3 font-popin items-center p-2 rounded ${
+                router.pathname === "/" ? "bg-white text-black" : ""
+              }`}
             >
               <RxDashboard />
-              Dashboard
+              <span className={`flex-1 ${!open && "hidden"}`}>Dashboard</span>
             </Link>
           </li>
           <li>
             <Link
               href="/email-phishing"
-              className="flex gap-3 font-popin items-center p-2 rounded"
+              className={`flex gap-3 font-popin items-center p-2 rounded ${
+                router.pathname === "/email-phishing"
+                  ? "bg-white text-black"
+                  : ""
+              }`}
             >
               <MdOutlineMarkunreadMailbox />
-              Gmail Phishing Detector
+              <span className={`flex-1 ${!open && "hidden"}`}>
+                Gmail Phishing Detector
+              </span>
             </Link>
           </li>
           <li>
             <Link
               href="/url-phishing"
-              className="flex gap-3 font-popin items-center p-2 rounded"
+              className={`flex gap-3 font-popin items-center p-2 rounded ${
+                router.pathname === "/url-phishing" ? "bg-white text-black" : ""
+              }`}
             >
               <TbWorldSearch />
-              URL Phising Detector
+              <span className={`flex-1 ${!open && "hidden"}`}>
+                URL Phising Detector
+              </span>
             </Link>
           </li>
         </ul>
