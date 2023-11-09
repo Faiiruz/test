196fddda755188c2ae4695f161cc006522b8669b
Faiiruz <fairuz.rafi.yadisa@gmail.com>
2023-11-09 18:19:40 +0700

    chnge pw

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,25 @@
 import Link from "next/link";
+import React, { useState, useRef, useEffect } from "react";
 
 const Navbar = () => {
-  // Gantilah dengan informasi pengguna yang sesuai
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   const user = {
     name: "John Doe",
     avatar: "/image/pp.png",
@@ -11,12 +29,27 @@ const Navbar = () => {
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-end items-center">
         <div className="flex items-center">
-          <span  className="font-bold">{user.name}</span>
-          <img
-            src={user.avatar}
-            alt={user.name}
-            className="h-10 rounded-full mr-2"
-          />
+          <div className="relative" ref={dropdownRef}>
+            <div
+              className="flex justify-center items-center gap-x-3  cursor-pointer"
+              onClick={() => setShowDropdown(!showDropdown)}
+            >
+              <img
+                src={user.avatar}
+                alt={user.name}
+                className="h-10 rounded-full mr-2"
+              />
+            </div>
+            {showDropdown && (
+              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg">
+                <ul className="py-2">
+                  <li className="px-4 py-2 hover:bg-gray-100">
+                    <p className="cursor-pointer font-popin">Change Password</p>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </nav>
---
+++ pages/changepassword.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ChangePassword = () => {
+  return <div>changepassword</div>;
+};
